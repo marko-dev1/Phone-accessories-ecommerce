8a31af9d6675bba4f4a061eb3451df0bddd9d5d6
Marko-dev1 <villiermarko@gmail.com>
2025-08-15 11:00:16 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLBATABASE,
+  database: process.env.MYSQLDATABASE,
   port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
