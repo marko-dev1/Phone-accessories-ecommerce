8023577903cf1897b52c90bc0db92c85e5e17ff4
Marko-dev1 <villiermarko@gmail.com>
2025-08-15 12:39:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306
+ 
+MYSQLHOST=mysql.railway.internal,
+MYSQLUSER=root,
+MYSQLPASSWORD=UtqviDYUsrfAipsjwIspemFUblDfZrpI,
+MYSQLDATABASE=railway,
+MYSQLPORT=3306
 });
+
 db.connect((err) => {
   if (err) {
     console.error('Database connection failed:', err);
