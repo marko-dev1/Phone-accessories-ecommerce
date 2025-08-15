560fc8ea256f77571350d83e5e91a07d479e475d
Marko-dev1 <villiermarko@gmail.com>
2025-08-15 13:21:26 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -19,7 +19,7 @@
 
 const mysql = require('mysql2/promise'); 
 require('dotenv').config();
-const mysql = require('mysql2');
+//const mysql = require('mysql2');
 
 // Use pool for stability
 const db = mysql.createPool({
